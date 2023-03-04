48f0e96c15a593ae66055abcaef6b393dd780932
max5mka <maks.bel98@mail.ru>
2023-03-04 15:03:50 +0700

    add simple figure drawing

--- GUI/MainWindow.axaml.cs
+++ GUI/MainWindow.axaml.cs
@@ -1,5 +1,7 @@
 using Avalonia.Controls;
+using Avalonia.Controls.Shapes;
 using Avalonia.Interactivity;
+using Avalonia.Media;
 
 namespace CrossTTD;
 
@@ -37,12 +39,35 @@ public partial class MainWindow : Window
 
     private void ButtonSquareOnClick(object? sender, RoutedEventArgs e)
     {
-	    throw new System.NotImplementedException();
+	    Rectangle rectangle = new Rectangle();
+	    SolidColorBrush mySolidColorBrush = new SolidColorBrush();
+	    mySolidColorBrush.Color = Color.FromArgb(255, 0, 255, 0);
+	    rectangle.Fill = mySolidColorBrush;
+	    rectangle.StrokeThickness = 2;
+	    rectangle.Stroke = Brushes.Black;
+
+	    rectangle.Width = 50;
+	    rectangle.Height = 25;
+
+	    thisCanv.Children.Add(rectangle);
     }
 
     private void ButtonCircleOnClick(object? sender, RoutedEventArgs e)
     {
-	    throw new System.NotImplementedException();
+	    //StackPanel myStackPanel = new StackPanel();
+	    Ellipse ellipse = new Ellipse();
+	    SolidColorBrush mySolidColorBrush = new SolidColorBrush();
+	    mySolidColorBrush.Color = Color.FromArgb(255, 255, 255, 0);
+	    ellipse.Fill = mySolidColorBrush;
+	    ellipse.StrokeThickness = 2;
+	    ellipse.Stroke = Brushes.Black;
+
+	    ellipse.Width = 100;
+	    ellipse.Height = 50;
+
+	    thisCanv.Children.Add(ellipse);
+	    //this.Content = myStackPanel;
+	    //throw new System.NotImplementedException();
     }
     private void ButtonCurvedOnClick(object? sender, RoutedEventArgs e)
     {
