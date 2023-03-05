f1d08246ecc92d0824dca8ab8cf0c2462545b121
Александра Грибова <sasha.gribova@bk.ru>
2023-03-05 23:26:39 +0700

    apdate

--- Geometry/obj/Release/netcoreapp3.1/Geometry.AssemblyInfo.cs
+++ Geometry/obj/Release/netcoreapp3.1/Geometry.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Logic")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Geometry")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Logic")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Logic")]
+[assembly: System.Reflection.AssemblyProductAttribute("Geometry")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Geometry")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
