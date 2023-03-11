f9bfd0ce36aeadd10528f4bebac730c13857bb68
Dmitry Leonenko <elspark10101@gmail.com>
2023-03-11 12:57:54 +0700

    Init

---
+++ Geometry/BezierCurve.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+
+namespace Geometry {
+    public class BezierCurve : IFigure {
+        public string PathData { get; }
+
+        public bool IsPointInFigure(Vector2 point, float eps) {
+            return true;
+        }
+
+        public void Move(Vector2 startPosition, Vector2 newPosition) { }
+
+        public void Rotate(float angle) { }
+
+        public void Scale(float scaleX, float scaleY) { }
+    }
+}
