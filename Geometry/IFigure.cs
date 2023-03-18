74ab7ca6a9f1532d9c002b74a27b670a31db48f2
name <email>
2023-03-18 10:40:04 +0700

    add scale

--- Geometry/IFigure.cs
+++ Geometry/IFigure.cs
@@ -1,12 +1,13 @@
-using System.Numerics;
+﻿using System.Numerics;
 
 namespace Geometry {
     public interface IFigure {
         string PathData { get; }
-        bool IsPointInFigure(Vector2 point, float eps);
+        bool IsPointInFigure(Vector2 point);
+        public bool isPointNearVerticle(Vector2 point);
         void Move(Vector2 startPosition, Vector2 newPosition);
         void Rotate(float angle);
-        void Scale(float scaleX, float scaleY);
+        void Scale(Vector2 point);
     }
 }
 
