c83404d847c73c1adff8efbf97aead73ab05ad4f
popkovroman <97338204+popkovroman@users.noreply.github.com>
2023-03-19 12:39:15 +0700

    add constuctor figure

--- IO/LoadFromSvg.cs
+++ IO/LoadFromSvg.cs
@@ -1,35 +1,56 @@
-﻿namespace IO;
+﻿using System.Numerics;
+
+namespace IO;
+
 public class ListFigureSvg
 {
     public string name;
-    public float x1_line;
-    public float x2_line;
-    public float y1_line;
-    public float y2_line;
-    public float x1_rect;
-    public float x2_rect;
-    public float y1_rect;
-    public float y2_rect;
     public float r1_ellipse;
     public float r2_ellipse;
-    public float y_ellipse;
-    public float x_ellipse;
-    public string path;
     public List<byte> stroke;
     public List<byte> fill;
+    public Vector2 P1_rect;
+    public Vector2 P2_rect;
+    public Vector2 P_ellipse;
+    public Vector2 P1_line;
+    public Vector2 P2_line;
+    public List<Vector2> points_bezie;
     
-    public ListFigureSvg(float x1Rect, float x2Rect, float y1Rect,float y2Rect)
+    // for rectangle
+    public ListFigureSvg(Vector2 P1, Vector2 P2, string id, List<byte> color_stroke, List<byte> color_fill)
     {
-        x1_rect = x1Rect;
-        x2_rect = x2Rect;
-        y1_rect = y1Rect;
-        y2_rect = y2Rect;
+        P1_rect = P1;
+        P2_rect = P2;
+        stroke = color_stroke;
+        fill = color_fill;
+        name = id; 
     }
-    public ListFigureSvg(float x1Rect, float x2Rect, float y1Rect)
+    // for ellipse
+    public ListFigureSvg(float rx, float ry, Vector2 P, string id, List<byte> color_stroke, List<byte> color_fill)
     {
-        x1_rect = x1Rect;
-        x2_rect = x2Rect;
-        y1_rect = y1Rect;
+        r1_ellipse = rx;
+        r2_ellipse = ry;
+        P_ellipse = P;
+        stroke = color_stroke;
+        fill = color_fill;
+        name = id; 
     }
-
+    // for line
+    public ListFigureSvg(Vector2 P1, Vector2 P2, string id, List<byte> color_stroke, List<byte> color_fill, bool flag)
+    {
+        P1_line = P1;
+        P2_line = P2;
+        stroke = color_stroke;
+        fill = color_fill;
+        name = id; 
+    }
+    
+    // for bezie
+    public ListFigureSvg(List<Vector2> point, string id, List<byte> color_stroke, List<byte> color_fill)
+    {
+        points_bezie = point;
+        stroke = color_stroke;
+        fill = color_fill;
+        name = id; 
+    }
 }
--- IO/Program.cs
+++ IO/Program.cs
@@ -1,10 +1,12 @@
-﻿using Svg;
+﻿using System.Numerics;
+using Svg;
+using Svg.Pathing;
 
 namespace IO {
     public class Svg
     {
         private string fileName = "template.svg";
-        private List<ListFigureSvg> asda;
+        private List<ListFigureSvg> figure_attributes;
         public List<ListFigureSvg> LoadFromSVG()
         {
             
@@ -29,7 +31,7 @@ namespace IO {
                 }
             }
 
-            return asda;
+            return figure_attributes;
         }
 
         private void Line(SvgElement svgElem)
@@ -41,8 +43,10 @@ namespace IO {
             var y2 = svgLine.EndY.Value;
             var svgColourFill = ((SvgColourServer)svgLine.Fill).Colour;
             var svgColourStroke = ((SvgColourServer)svgLine.Stroke).Colour;
-            ListFigureSvg Array = new ListFigureSvg(1,1,1);// To do
-            asda.Add(Array);
+            List<byte> line_fill = new List<byte>() { svgColourFill.A, svgColourFill.R, svgColourFill.G, svgColourFill.B };
+            List<byte> line_stroke = new List<byte>() { svgColourStroke.A, svgColourStroke.R, svgColourStroke.G, svgColourStroke.B };
+            ListFigureSvg Array = new ListFigureSvg(new Vector2(x1,y1),new Vector2(x2,y2),"line", line_stroke, line_fill, true);
+            figure_attributes.Add(Array);
         }
 
         private void Rectangle(SvgElement svgElem)
@@ -54,8 +58,10 @@ namespace IO {
             var Width = svgRectangle.Width.Value;
             var svgColourFill = ((SvgColourServer)svgRectangle.Fill).Colour;
             var svgColourStroke = ((SvgColourServer)svgRectangle.Stroke).Colour;
-            ListFigureSvg Array = new ListFigureSvg(1,1,1); // To do
-            asda.Add(Array);
+            List<byte> rect_fill = new List<byte>() { svgColourFill.A, svgColourFill.R, svgColourFill.G, svgColourFill.B };
+            List<byte> rect_stroke = new List<byte>() { svgColourStroke.A, svgColourStroke.R, svgColourStroke.G, svgColourStroke.B };
+            ListFigureSvg Array = new ListFigureSvg(new Vector2(x_rect,y_rect),new Vector2(x_rect+Width, y_rect+Height),"rectangle", rect_stroke, rect_fill); // To do
+            figure_attributes.Add(Array);
         }
 
         private void Ellipse(SvgElement svgElem)
@@ -67,25 +73,31 @@ namespace IO {
             var y = svgEllipse.CenterY.Value;
             var svgColourFill = ((SvgColourServer)svgEllipse.Fill).Colour;
             var svgColourStroke = ((SvgColourServer)svgEllipse.Stroke).Colour;
-            ListFigureSvg Array = new ListFigureSvg(1, 1, 1);// To do
-            asda.Add(Array);
+            List<byte> ellipse_fill = new List<byte>() { svgColourFill.A, svgColourFill.R, svgColourFill.G, svgColourFill.B };
+            List<byte> ellipse_stroke = new List<byte>() { svgColourStroke.A, svgColourStroke.R, svgColourStroke.G, svgColourStroke.B };
+            ListFigureSvg Array = new ListFigureSvg(rx,ry,new Vector2(x,y),"ellipse", ellipse_stroke, ellipse_fill);
+            figure_attributes.Add(Array);
         }
-
+        //test bezie
         private void Path(SvgElement svgElem)
         {
             SvgPath? svgPath = svgElem as SvgPath;
-            string a = "";
             //string fill ="";
-            for (int i = 0; i < svgPath.PathData.Count; i++)
+            List<Vector2> points = new List<Vector2>();
+            SvgCubicCurveSegment a = (SvgCubicCurveSegment)svgPath.PathData[1];
+            if (svgPath.PathData.Count == 3)
             {
-                a += svgPath.PathData[i] + " ";
-
+                points.Add(new Vector2(svgPath.PathData[0].End.X, svgPath.PathData[0].End.Y));
+                points.Add(new Vector2(a.FirstControlPoint.X, a.FirstControlPoint.Y));
+                points.Add(new Vector2(a.SecondControlPoint.X, a.SecondControlPoint.Y));
+                points.Add(new Vector2(a.End.X, a.End.Y));
             }
-
             var svgColourFill = ((SvgColourServer)svgPath.Fill).Colour;
             var svgColourStroke = ((SvgColourServer)svgPath.Stroke).Colour;
-            ListFigureSvg Array = new ListFigureSvg(1, 1,1);// To do
-            asda.Add(Array);
+            List<byte> path_fill = new List<byte>() { svgColourFill.A, svgColourFill.R, svgColourFill.G, svgColourFill.B };
+            List<byte> path_stroke = new List<byte>() { svgColourStroke.A, svgColourStroke.R, svgColourStroke.G, svgColourStroke.B };
+            ListFigureSvg Array = new ListFigureSvg(points,"bezie", path_stroke, path_fill);
+            figure_attributes.Add(Array);
         }
 
         public void SaveToSVG()
