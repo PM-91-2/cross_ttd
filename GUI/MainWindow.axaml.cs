60ff4fafc0882402813a1eec2db3a9965b838e66
name <email>
2023-03-20 15:29:55 +0700

    upd

--- GUI/MainWindow.axaml.cs
+++ GUI/MainWindow.axaml.cs
@@ -13,6 +13,7 @@ using Avalonia.Media;
 using Geometry;
 using IO;
 using Rectangle = Geometry.Rectangle;
+using Ellipse = Geometry.Ellipse;
 
 namespace CrossTTD;
 
@@ -37,11 +38,10 @@ public partial class MainWindow : Window, INotifyPropertyChanged
     private List<bool> rotateFlagArray = new List<bool>();
     private List<bool> selectedFlagArray = new List<bool>();
 
-    private List<bool> IsActive = new List<bool>();
     private int _pointflag = -1;
 
-    private Vector2 firstPoint;
-    private Vector2 secondPoint;
+    private Vector2 firstPoint = new Vector2(0, 0);
+    private Vector2 secondPoint = new Vector2(0, 0);
     private EnumState State = EnumState.Free;
 
     private Point initialRotatingPoint = new Point();
@@ -51,8 +51,6 @@ public partial class MainWindow : Window, INotifyPropertyChanged
     public MainWindow()
     {
         InitializeComponent();
-        firstPoint = new Vector2(0, 0);
-        secondPoint = new Vector2(0, 0);
     }
 
     private void ButtonProfilesOnClick(object? sender, RoutedEventArgs e)
@@ -65,12 +63,16 @@ public partial class MainWindow : Window, INotifyPropertyChanged
         throw new NotImplementedException();
     }
 
-    private void ButtonFilesOnClick(object? sender, RoutedEventArgs e) {
+    private void ButtonFilesOnClick(object? sender, RoutedEventArgs e)
+    {
         IO.Svg svgObj = new IO.Svg();
         List<ListFigureSvg> attrs = svgObj.LoadFromSVG();
-        foreach(ListFigureSvg attr in attrs) {
-            switch (attr.name) {
-                case "rectangle": CreateRectangleFromImport(attr.points[0], attr.points[1], attr.points[2], attr.points[3],
+        foreach (ListFigureSvg attr in attrs)
+        {
+            switch (attr.name)
+            {
+                case "rectangle":
+                    CreateRectangleFromImport(attr.points[0], attr.points[1], attr.points[2], attr.points[3],
                     attr.fill, attr.stroke, true); break;
             }
         }
@@ -78,18 +80,10 @@ public partial class MainWindow : Window, INotifyPropertyChanged
 
     private void ButtonToolsOnClick(object? sender, RoutedEventArgs e)
     {
-        List<ListFigureSvg> exportArray = new List<ListFigureSvg>();
-        foreach (IFigure figure in figureArray) {
-            exportArray.Add(figure.ExportData);
-        }
-        IO.Svg svgObj = new IO.Svg();
-        svgObj.SaveToSVG(exportArray);
-    }
-
-    private void ButtonLineOnClick(object? sender, RoutedEventArgs e)
-    {
         throw new NotImplementedException();
     }
+
+  
 
     private List<Path> DrawFigure(IFigure figure, List<byte> argb_fill, List<byte> arbg_stroke, Boolean needBoundingBox)
     {
@@ -99,7 +93,6 @@ public partial class MainWindow : Window, INotifyPropertyChanged
         Thread.CurrentThread.CurrentCulture = customCulture;
 
         var pathFigure = new Path();
-        var tmp = figure.PathData;
 
         pathFigure.Data = Avalonia.Media.Geometry.Parse(figure.PathData);
         SolidColorBrush mySolidColorBrush = new SolidColorBrush();
@@ -114,7 +107,6 @@ public partial class MainWindow : Window, INotifyPropertyChanged
         var pathBounds = new Path();
         if (needBoundingBox)
         {
-            var tmpbounds = figure.BoundsData;
             pathBounds.Data = Avalonia.Media.Geometry.Parse(figure.BoundsData);
             SolidColorBrush mySolidColorBrushBounds = new SolidColorBrush();
             mySolidColorBrushBounds.Color = Color.FromArgb(255, 0, 0, 0);
@@ -138,31 +130,23 @@ public partial class MainWindow : Window, INotifyPropertyChanged
         }
     }
 
+    private void ButtonLineOnClick(object? sender, RoutedEventArgs e)
+    {
+        State = EnumState.Line;
+    }
     private void ButtonSquareOnClick(object? sender, RoutedEventArgs e)
     {
-        // CreateRectangle(new Vector2(1f, 1f), new Vector2(300f, 400f), new List<byte>() {255, 255, 255, 0},
-        //     new List<byte>() {255, 90, 255, 0});
         State = EnumState.Square;
     }
 
     private void ButtonCircleOnClick(object? sender, RoutedEventArgs e)
     {
-        Ellipse ellipse = new Ellipse();
-        SolidColorBrush mySolidColorBrush = new SolidColorBrush();
-        mySolidColorBrush.Color = Color.FromArgb(255, 255, 255, 0);
-        ellipse.Fill = mySolidColorBrush;
-        ellipse.StrokeThickness = 2;
-        ellipse.Stroke = Brushes.Black;
-
-        ellipse.Width = 100;
-        ellipse.Height = 50;
-
-        ThisCanv.Children.Add(ellipse);
+        State = EnumState.Ellipse;
     }
-    //M150,100 L 200,150 150,200 100,150 z
+
     private void ButtonCurvedOnClick(object? sender, RoutedEventArgs e)
     {
-        throw new NotImplementedException();
+        State = EnumState.Curve;
     }
 
     protected void OnCanvasPointerPressed(object? sender, PointerPressedEventArgs e)
@@ -177,9 +161,9 @@ public partial class MainWindow : Window, INotifyPropertyChanged
         {
             for (int i = 0; i < figureArray.Count; i++)
             {
-                var tmp_selectedFigure = selectedFlagArray[i];
+                var tmpSelectedFigure = selectedFlagArray[i];
                 selectedFlagArray[i] = figureArray[i].IsPointInFigure(currentPoint);
-                if (tmp_selectedFigure != selectedFlagArray[i])
+                if (tmpSelectedFigure != selectedFlagArray[i])
                 {
                     UpdateCanvas();
                 }
@@ -215,18 +199,26 @@ public partial class MainWindow : Window, INotifyPropertyChanged
 
     protected void OnCanvasPointerReleased(object? sender, PointerReleasedEventArgs e)
     {
-        if (IsPressed == true)
+        if (IsPressed)
         {
             secondPoint.X = (float)e.GetCurrentPoint(ThisCanv).Position.X;
             secondPoint.Y = (float)e.GetCurrentPoint(ThisCanv).Position.Y;
 
             IsPressed = false;
-            if (State == EnumState.Square)
+            switch (State)
             {
-                CreateRectangleFromTool(firstPoint, secondPoint, new List<byte>() { 255, 130, 215, 0 },
+                case EnumState.Square:
+                    CreateRectangleFromTool(firstPoint, secondPoint, new List<byte>() { 255, 130, 215, 0 },
                     new List<byte>() { 255, 90, 90, 90 }, true);
-                State = EnumState.Free;
+                    State = EnumState.Free;
+                    break;
+                case EnumState.Ellipse:
+                    CreateEllipse(firstPoint, secondPoint, new List<byte>() { 255, 255, 255, 0 },
+                        new List<byte>() { 255, 90, 255, 0 }, true);
+                    State = EnumState.Free;
+                    break;
             }
+            
         }
 
         for (int i = 0; i < figureArray.Count; i++)
@@ -301,6 +293,17 @@ public partial class MainWindow : Window, INotifyPropertyChanged
         selectedFlagArray.Add(false);
     }
 
+    public void CreateEllipse(Vector2 point1, Vector2 point2, List<byte> argb_fill, List<byte> arbg_stroke, Boolean needBoundingBox)
+    {
+        IFigure rectangle = new Ellipse(point1, point2);
+        DrawFigure(rectangle, argb_fill, arbg_stroke, needBoundingBox);
+        figureArray.Add(rectangle);
+        moveFlagArray.Add(false);
+        scaleFlagArray.Add(false);
+        rotateFlagArray.Add(false);
+        selectedFlagArray.Add(false);
+    }
+    
     private void UpdateCanvas()
     {
         ThisCanv.Children.Clear();
@@ -310,7 +313,8 @@ public partial class MainWindow : Window, INotifyPropertyChanged
     private void UpdateFigure(int index)
     {
         ThisCanv.Children.RemoveAt(index);
-        var pathFigure = DrawFigure(figureArray[index], figureArray[index].ArgbFill, figureArray[index].ArgbStroke, selectedFlagArray[index]);
+        var pathFigure = DrawFigure(figureArray[index], new List<byte>() { 255, 255, 255, 0 },
+            new List<byte>() { 255, 90, 255, 0 }, selectedFlagArray[index]);
         Grid grid = new Grid();
         grid.Children.Add(pathFigure[0]);
         grid.Children.Add(pathFigure[1]);
